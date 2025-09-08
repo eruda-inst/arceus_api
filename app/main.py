93a512e6361e2a83d2d61dad99ee963ee441000a
GabrielC <gabriel.lcifba@gmail.com>
2025-09-08 16:55:43 -0300

    feat: docker

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.api.v1.routes.aggregator import router as aggregator_router
 app = FastAPI(
     title="API do Roberto",
     description="Atua como um aggregator, simplificando integrações entre a API berta do OpaSuite e a API aberta do IXCSoft.",
-    version="0.3.1.1"
+    version="0.32.1"
 )
 
 app.include_router(router=aggregator_router, prefix="/api/v1", tags=["aggregator"])
