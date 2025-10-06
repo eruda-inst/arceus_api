302b561c79018d45d22bf0a7720c8cdfee3cce73
GabrielC <gabriel.lcifba@gmail.com>
2025-10-06 17:40:05 -0300

    feat: pequenas melhorias na mensagem de /

--- app/main.py
+++ app/main.py
@@ -6,25 +6,29 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="API do Bot",
     description="Atua como um aggregator, simplificando integrações entre a API do OpaSuite, a API do IXCSoft e a API do 7AZ.",
-    version="0.55.4",
+    version="0.55.5",
 )
 
-app.include_router(router=api_v1_router, prefix="/api/v1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://reddator.newnet.com.br/"],
+    allow_origins=["https://reddator.newnet.com.br"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+app.include_router(router=api_v1_router, prefix="/api/v1")
+
 
 @app.get(
     path="/",
     summary="Rota padrão, mostra mensagens de boas vindas e URL para acessar página de documentação.",
+    response_description="Mensagem de boas-vindas com link para documentação",
 )
 def index():
-    return (
-        "Bem-vindo(a). Para documentação, acesse: https://reddator.newnet.com.br/docs."
-    )
+    return {
+        "mensagem": "Bem-vindo(a) à API do Bot",
+        "docs_url": "https://reddator.newnet.com.br/docs",
+        "redoc_url": "https://reddator.newnet.com.br/redoc",
+    }
