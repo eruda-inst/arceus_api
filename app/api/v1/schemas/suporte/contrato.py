24a3b40165906e75ce80037f897eac88ca30b87c
GabrielC <gabriel.lcifba@gmail.com>
2025-10-09 17:13:51 -0300

    docs: schemas -> model.config.examples

--- app/api/v1/schemas/suporte/contrato.py
+++ app/api/v1/schemas/suporte/contrato.py
@@ -14,8 +14,66 @@ class Contrato(BaseModel):
     data_vencimento: str = Field(description="Data de vencimento do contrato.")
     mac_onu: str = Field(description="MAC Address da ONU.")
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": 324,
+                    "id_login": 133,
+                    "id_cliente": 2321,
+                    "status": utils.enums.rotulos.StatusContratoRot.PRE_CONTRATO,
+                    "contrato": "NEWNET PADRAO - 250MB - 06/2025",
+                    "valor": 99.9,
+                    "data_vencimento": "2025-06-01",
+                    "mac_onu": "DD18b3d3e400",
+                }
+            ]
+        }
+    }
+
 
 class ContratoListOut(BaseModel):
     data: List[Contrato] = Field(description="Lista de contratos")
     meta: misc.Meta
     links: misc.Links
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "data": [
+                        {
+                            "id": 8516,
+                            "id_login": 8247,
+                            "id_cliente": 7921,
+                            "status": utils.enums.rotulos.StatusContratoRot.ATIVO,
+                            "contrato": "NEWNET BASICO - 150MB - 06/2025",
+                            "valor": 50.0,
+                            "data_vencimento": "2025-10-20",
+                            "mac_onu": "DD16E6112683",
+                        },
+                        {
+                            "id": 11807,
+                            "id_login": 11582,
+                            "id_cliente": 7921,
+                            "status": utils.enums.rotulos.StatusContratoRot.ATIVO,
+                            "contrato": "NEWNET BASICO - 150MB - 06/2025",
+                            "valor": 55.0,
+                            "data_vencimento": "2025-10-10",
+                            "mac_onu": "GPON00a31548",
+                        },
+                    ],
+                    "meta": {
+                        "total": 2,
+                        "page": 1,
+                        "per_page": 10,
+                    },
+                    "links": {
+                        "next": "null",
+                        "prev": "null",
+                        "self": "/api/v1/suporte/contratos?protocolo=NWT202537245&page=1&per_page=10",
+                    },
+                }
+            ]
+        }
+    }
