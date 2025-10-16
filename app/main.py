c2f8b307f3711b06b6f25904718844a2a4d8b1ec
GabrielC <gabriel.lcifba@gmail.com>
2025-10-16 11:21:48 -0300

    fix: migrações com alembic

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app = FastAPI(
     description="""
     API oficial Newnet/Eruda - Simplifica integrações entre a API (Application Programming Interface) da OpaSuite, da IXCSoft e da 7AZ.
     """,
-    version="0.60.0",
+    version="0.60.1",
     lifespan=lifespan,
 )
 
