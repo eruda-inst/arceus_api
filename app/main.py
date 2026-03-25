05f2206641aa7bb5792ea69c45f7e862c1b2cc20
zolppy <zolppy.me@gmail.com>
2026-03-25 10:05:41 -0300

    fix: removido lifespan

--- app/main.py
+++ app/main.py
@@ -1,28 +1,8 @@
 from typing import Dict
 from app.api import api_v1_router
+from app.api.v1 import middlewares
 from fastapi import FastAPI, Request
-from contextlib import asynccontextmanager
-from app.api.v1 import middlewares, database
 from fastapi.middleware.cors import CORSMiddleware
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """
-    Gerenciador de ciclo de vida para a aplicação FastAPI.
-
-    Este gerenciador de contexto é executado durante o ciclo de vida da aplicação.
-    Antes da aplicação começar a receber requisições, ele garante que as
-    tabelas do banco de dados sejam criadas. Ao encerrar a aplicação,
-    ele executa as ações de finalização.
-
-    Args:
-        app (FastAPI): A instância da aplicação FastAPI.
-    """
-    await database.criar_tabelas()
-    print("Tabelas criadas com sucesso!")
-    yield
-    print("Encerrando aplicação...")
 
 
 app = FastAPI(
@@ -30,21 +10,18 @@ app = FastAPI(
     description="""
     API oficial Newnet/Eruda - Simplifica integrações entre a API (Application Programming Interface) da OpaSuite, da IXCSoft e da 7AZ.
     """,
-    version="0.65.9",
-    lifespan=lifespan,
+    version="0.66.1",
+    routes=api_v1_router.routes,
+    middlewar=middlewares.LogMiddleware,
 )
-
-app.add_middleware(middlewares.LogMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://reddator.newnet.com.br"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.include_router(router=api_v1_router, prefix="/api/v1")
 
 
 @app.get(
