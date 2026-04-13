25c459dae9980fc79404906fff6d5967ab62741f
zolppy <zolppy.me@gmail.com>
2026-04-13 14:30:01 -0300

    fix: versão

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="Aggregator",
     description="API oficial Newnet/Eruda - Simplifica integrações entre a API (Application Programming Interface) da OpaSuite, da IXCSoft e da 7AZ.",
-    version="0.73.0",
+    version="0.75.0",
     routes=api_v1_router.routes,
 )
 
