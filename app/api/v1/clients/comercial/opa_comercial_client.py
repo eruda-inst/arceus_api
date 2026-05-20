53a5ceec9a2e2a4e5e2248fb9f6b071490e0040e
zolppy <zolppy.me@gmail.com>
2026-05-20 15:35:45 -0300

    refactor: usados decorators de métodos corretamente

--- app/api/v1/clients/comercial/opa_comercial_client.py
+++ app/api/v1/clients/comercial/opa_comercial_client.py
@@ -2,9 +2,6 @@ from .. import opa_client
 
 
 class ComercialOpaCliente(opa_client.OpaCliente):
-    def __init__(self) -> None:
-        super().__init__()
-
     @classmethod
     async def cliente_existe(cls, cpf_cnpj_limpo: str) -> bool:
         payload = {"filter": {"cpf_cnpj": cpf_cnpj_limpo}, "options": {"limit": 1}}
