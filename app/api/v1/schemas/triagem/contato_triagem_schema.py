2170ce558921e0e0716722e5193436ddb9daadd3
zolppy <zolppy.me@gmail.com>
2026-05-20 12:01:44 -0300

    refactor: esquema contato_triagem mais idiomático

--- app/api/v1/schemas/triagem/contato_triagem_schema.py
+++ app/api/v1/schemas/triagem/contato_triagem_schema.py
@@ -1,15 +1,13 @@
 from pydantic import BaseModel, Field
 
 
-class Contato(BaseModel):
+class ContatoUpdate(BaseModel):
     telefone_celular: str = Field(
         description="Número do celular do cliente.", examples=["(12) 93456-7890"]
     )
 
 
-class ContatoUpdate(Contato):
-    pass
-
-
-class ContatoOut(Contato):
-    pass
+class ContatoOut(BaseModel):
+    telefone_celular: str = Field(
+        description="Número do celular do cliente.", examples=["(12) 93456-7890"]
+    )
