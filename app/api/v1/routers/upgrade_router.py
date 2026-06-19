a5d4bb3d5e5561c8f09935ffbf48bd5220f3f759
zolppy <zolppy.me@gmail.com>
2026-06-19 16:54:23 -0300

    fix: query params em GET /api/v1/upgrade/planos_sugeridos

--- app/api/v1/routers/upgrade_router.py
+++ app/api/v1/routers/upgrade_router.py
@@ -11,12 +11,14 @@ upgrade_router = APIRouter(prefix="/upgrade", tags=["Upgrade"])
 )
 async def get_planos_sugeridos(
     id_cliente: Annotated[int, Query(get=1, description="ID do cliente no IXC.")],
-    page: Annotated[int | None, Query(ge=1, description="Número da página.")] = 1,
-    per_page: Annotated[int | None, Query(ge=1, description="Itens por página.")] = 10,
+    pagina: Annotated[int | None, Query(ge=1, description="Número da página.")] = 1,
+    itens_por_pagina: Annotated[
+        int | None, Query(ge=1, description="Itens por página.")
+    ] = 10,
 ) -> schemas.PlanoSugeridoListOut:
     """
     Sugere planos de venda oficiais de acordo com planos de venda desatualizados, para cada cadastro associado a um cliente.
     """
     return await services.UpgradeService.get_planos_sugeridos(
-        id_cliente=id_cliente, page=page, per_page=per_page
+        id_cliente=id_cliente, page=pagina, per_page=itens_por_pagina
     )
