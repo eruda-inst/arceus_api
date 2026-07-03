2161df3b7fdae858674ca9a4169313ef0bd04334
zolppy <zolppy.me@gmail.com>
2026-07-03 17:34:44 -0300

    fix: POST .../suporte/atendimentos

--- app/api/v1/services/suporte_service.py
+++ app/api/v1/services/suporte_service.py
@@ -285,8 +285,15 @@ class SuporteService(service_service.Service):
             payload = atendimento.model_dump()
             res = await clients.IXCCliente.post(endpoint=endpoint, payload=payload)
             data = res.get("data", None)
+            if not data:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Não foi possível abrir o atendimento.",
+                )
 
             return schemas.AtendimentoCreate(id=data["id"])
+        except HTTPException:
+            raise
         except Exception as e:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
