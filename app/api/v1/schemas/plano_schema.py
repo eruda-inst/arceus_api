b25de30d180f0575ba11e3301550d15d24f483ce
zolppy <zolppy.me@gmail.com>
2026-07-03 17:05:51 -0300

    fix: padronização de schemas

--- app/api/v1/schemas/plano_schema.py
+++ app/api/v1/schemas/plano_schema.py
@@ -1,24 +1,24 @@
-from . import misc_schema
+from . import Meta
 from pydantic import BaseModel, Field
 
 
 class PlanoSugeridoOut(BaseModel):
     nome_plano_atual: str = Field(
-        description="Nome do plano de venda antigo.",
-        examples=["NEWNET PADRAO - 250MB - 06/2025"],
+        description="Nome do plano antigo.",
+        examples=["Nome do plano antigo"],
     )
     valor_plano_atual: float = Field(
-        description="Valor do plano de venda antigo.", examples=[99.99]
+        description="Valor do plano antigo.", examples=[12.34]
     )
     nome_plano_sugerido: str = Field(
-        description="Nome do plano de venda sugerido.",
-        examples=["NEWNET PADRAO - 250MB - 06/2025"],
+        description="Nome do plano sugerido.",
+        examples=["Nome do plano sugerido"],
     )
     valor_plano_sugerido: float = Field(
-        description="Valor do plano de venda sugerido.", examples=[99.99]
+        description="Valor do plano sugerido.", examples=[12.34]
     )
 
 
 class PlanoSugeridoListOut(BaseModel):
-    data: list[PlanoSugeridoOut] = Field(description="Lista de contratos.")
-    meta: misc_schema.Meta
+    data: list[PlanoSugeridoOut]
+    meta: Meta
