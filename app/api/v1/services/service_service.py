a7bf09fcb0adb129d1ddac2c4e669d051248312c
zolppy <zolppy.me@gmail.com>
2026-07-06 14:32:48 -0300

    fix: padronização de detalhes de erros

--- app/api/v1/services/service_service.py
+++ app/api/v1/services/service_service.py
@@ -94,5 +94,5 @@ class Service:
         except Exception as e:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"Erro interno: {e}",
+                detail=f"Erro interno desconhecido: {e}",
             )
