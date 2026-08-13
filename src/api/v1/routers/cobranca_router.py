44f44bbfb0140b89177ce238d0b5ddb3dbcf9491
Zolppy <zolppy.me@gmail.com>
2026-08-13 11:38:59 -0300

    fix: convenção de nomenclatura de endpoints

--- src/api/v1/routers/cobranca_router.py
+++ src/api/v1/routers/cobranca_router.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Query
 
 from .. import schemas, services, utils
 
@@ -6,11 +8,11 @@ cobranca_router = APIRouter(prefix="/cobranca", tags=["Cobrança"])
 
 
 @cobranca_router.get(
-    path="/faturas_abertas", summary="Obtém faturas abertas de um cliente"
+    path="/faturas-abertas", summary="Obtém faturas abertas de um cliente"
 )
 async def get_faturas_abertas(
-    protocolo: utils.Protocolo | None = None,
-    cnpj_cpf: utils.CnpjCpf | None = None,
+    # IDs NonNegativeInt, pois o IXC é quebrado
+    id_contrato: Annotated[int, Query(ge=0, description="ID do contrato")],
     pagina: utils.Pagina | None = 1,
     itens_por_pagina: utils.ItensPorPagina = 15,
 ) -> schemas.ListOutSchema[schemas.FaturaOutSchema]:
@@ -18,15 +20,14 @@ async def get_faturas_abertas(
     Obtém faturas abertas de um cliente, através de protocolo de atendimento ou CPF/CNPJ
     """
     return await services.FinanceiroService.get_faturas_abertas(
-        protocolo=protocolo,
-        cnpj_cpf=cnpj_cpf,
+        id_contrato=id_contrato,
         pagina=pagina,
         itens_por_pagina=itens_por_pagina,
     )
 
 
 @cobranca_router.get(
-    path="/faturas_vencidas", summary="Obtém faturas vencidas de um cliente"
+    path="/faturas-vencidas", summary="Obtém faturas vencidas de um cliente"
 )
 async def get_faturas_vencidas(
     protocolo: utils.Protocolo | None = None,
