0c76772e9c533ba79b9744e5073477db450e936a
Zolppy <zolppy.me@gmail.com>
2026-09-16 10:37:30 -0300

    feat: adicionadas integrações com ixcacs à rota vila

--- src/api/v1/routers/vila_router.py
+++ src/api/v1/routers/vila_router.py
@@ -7,10 +7,12 @@ from .. import deps, schemas, services
 
 vila_router = APIRouter(prefix="/vila", tags=["Vila"])
 
+
 NumeroResidencia = Annotated[int, Query(ge=1, description="Número da residência")]
 Pppoe = Annotated[str, Query(description="PPPOE associado ao cliente")]
-# IDs NonNegativeInt, pois o IXC é quebrado
+# IDs NonNegativeInt, because IXC
 IdLogin = Annotated[NonNegativeInt, Query(description="ID login associado ao cliente")]
+GetCredsDeps = Annotated[bool, Depends(deps.get_creds)]
 
 
 @vila_router.get(
@@ -18,7 +20,7 @@ IdLogin = Annotated[NonNegativeInt, Query(description="ID login associado ao cli
     summary="Obtém contrato de um cliente",
 )
 async def get_contrato_by_numero_residencia(
-    _: Annotated[bool, Depends(deps.get_creds)],
+    _: GetCredsDeps,
     numero_residencia: Annotated[int, Path(ge=1, description="Número da residência")],
 ) -> schemas.VilaContratoOutSchema:
     """
@@ -29,7 +31,7 @@ async def get_contrato_by_numero_residencia(
 
 @vila_router.get(path="/contrato/pppoe/{pppoe}", summary="Obtém contrato de um cliente")
 async def get_contrato_by_ppoe(
-    _: Annotated[bool, Depends(deps.get_creds)],
+    _: GetCredsDeps,
     pppoe: Annotated[str, Path(description="PPPOE do login")],
 ) -> schemas.VilaContratoOutSchema:
     """
@@ -38,21 +40,27 @@ async def get_contrato_by_ppoe(
     return await services.VilaService.get_contrato(pppoe=pppoe)
 
 
-@vila_router.get(path="/dns-server", summary="Obtém servidor DNS de um cliente")
-async def get_dns_server(
-    _: Annotated[bool, Depends(deps.get_creds)], id_login: IdLogin
-) -> schemas.DnsServerOut:
+@vila_router.get(path="/servidor-dns", summary="Obtém servidor DNS de um cliente")
+async def get_dns_server(_: GetCredsDeps, id_login: IdLogin) -> schemas.DnsServerOut:
     """
     Obtém servidor DNS de um cliente, a partir do ID de login
     """
     return await services.SuporteService.get_dns_server(id_login=id_login)
 
 
+@vila_router.get(path="/tem-ipv6", summary="Verifica se o cliente possui IPV6")
+async def get_has_ipv6(_: GetCredsDeps, id_login: IdLogin) -> dict[str, bool]:
+    """
+    Verifica se o cliente possui IPV6 ativo, a partir do ID de login
+    """
+    return await services.SuporteService.get_has_ipv6(id_login=id_login)
+
+
 @vila_router.get(
     path="/status-conexao", summary="Obtém status da conexão de um cliente"
 )
 async def get_status_conexao(
-    _: Annotated[bool, Depends(deps.get_creds)], id_login: IdLogin
+    _: GetCredsDeps, id_login: IdLogin
 ) -> schemas.StatusConexaoOutSchema:
     """
     Obtém status da conexão de um cliente, através do ID de login
@@ -62,7 +70,7 @@ async def get_status_conexao(
 
 @vila_router.get(path="/status-onu", summary="Obtém status da ONU de um cliente")
 async def get_status_onu(
-    _: Annotated[bool, Depends(deps.get_creds)], id_login: IdLogin
+    _: GetCredsDeps, id_login: IdLogin
 ) -> schemas.StatusOnuOutSchema:
     """
     Obtém status da ONU de um cliente, através do ID de login
@@ -74,7 +82,7 @@ async def get_status_onu(
     path="/atendimentos", summary="Obtém atendimentos abertos de um cliente"
 )
 async def get_atendimentos(
-    _: Annotated[bool, Depends(deps.get_creds)],
+    _: GetCredsDeps,
     id_login: IdLogin,
     pagina: Annotated[int, Query(ge=1, description="Número da página")] = 1,
     itens_por_pagina: Annotated[int, Query(ge=1, description="Itens por página")] = 10,
@@ -91,7 +99,7 @@ async def get_atendimentos(
 
 @vila_router.post(path="/limpar-mac", summary="Limpa MAC Address de um cliente")
 async def post_limpar_mac(
-    _: Annotated[bool, Depends(deps.get_creds)], id_login: IdLogin
+    _: GetCredsDeps, id_login: IdLogin
 ) -> schemas.MensagemOutSchema:
     """
     Limpa MAC Address de um cliente, através do ID de login
@@ -101,7 +109,7 @@ async def post_limpar_mac(
 
 @vila_router.post(path="/desconectar-cliente", summary="Desconecta um cliente")
 async def post_desconectar_cliente(
-    _: Annotated[bool, Depends(deps.get_creds)], id_login: IdLogin
+    _: GetCredsDeps, id_login: IdLogin
 ) -> schemas.MensagemOutSchema:
     """
     Desconecta um cliente, através do ID de login
@@ -113,7 +121,7 @@ async def post_desconectar_cliente(
     path="/atendimentos", summary="Obtém atendimentos abertos de um cliente"
 )
 async def post_atendimentos(
-    _: Annotated[bool, Depends(deps.get_creds)],
+    _: GetCredsDeps,
     atendimento: Annotated[
         schemas.AtendimentoInSchema, Body(description="Dados do atendimento")
     ],
@@ -122,3 +130,20 @@ async def post_atendimentos(
     Abre ticket de atendimento, através de dados do atendimento
     """
     return await services.VilaService.post_atendimentos(atendimento=atendimento)
+
+
+@vila_router.patch(path="/dados-wifi", summary="Atualiza dados wifi de um cliente")
+async def patch_dados_wifi(
+    _: GetCredsDeps,
+    id_login: IdLogin,
+    ssid: Annotated[str | None, Body(description="Novo SSID da rede wifi")] = None,
+    senha_ssid: Annotated[
+        str | None, Body(description="Nova senha da rede wifi")
+    ] = None,
+) -> schemas.MensagemOutSchema:
+    """
+    Atualiza dados da rede wifi 2.5G e 5G de um cliente
+    """
+    return await services.SuporteService.patch_dados_wifi(
+        id_login=id_login, ssid=ssid, senha_ssid=senha_ssid
+    )
